3ae8ffb61a7c81f271916ad3c6b6ee6340268779
Crowdbotics <engineering@crowdbotics.com>
2023-04-20 00:16:02 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "hidden-breeze-dev-77748",
+  url: "https://hidden-breeze-dev-77748.botics.co",
+  api: "https://hidden-breeze-dev-77748.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
